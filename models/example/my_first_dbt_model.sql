96838bc912d075dbd04125b0dcdd4304dc405f07
Jiawen Zhu <jiawenzhu@Jiawens-MacBook-Air.local>
2024-01-13 14:45:46 -0600

    update the my_first_dbt_model.sql. select data from BOOKINGS_1 table

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -7,21 +6,9 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
+-- Replace the query below with the one selecting from BOOKINGS_1
 select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+from BOOKINGS_1
+--
